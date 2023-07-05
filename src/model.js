f4a5fadd655a64f1482a8ca7a253232152e4a40d
tvalentijn <tjerk.valentijn@visma.com>
2023-07-05 10:16:13 +0200

    version without pattern matching

--- src/model.js
+++ src/model.js
@@ -1,20 +1,24 @@
-import daggy from "daggy";
-import { __, cond, lte } from "ramda";
-
 // @see http://learnyouahaskell.com/syntax-in-functions
 
 // Define the BMI type
-export const BMI = daggy.taggedSum("BMI", {
-  Underweight: [],
-  Normal: [],
-  Overweight: [],
-  Obese: [],
-});
+export const BMI = {
+  Underweight: "Underweight",
+  Normal: "Normal",
+  Overweight: "Overweight",
+  Obese: "Obese",
+};
 
-// Pattern match with Ramda.cond to determine which BMI category is applicable
-export const makeBmi = cond([
-  [lte(__, 18.5), () => BMI.Underweight],
-  [lte(__, 25), () => BMI.Normal],
-  [lte(__, 30), () => BMI.Overweight],
-  [() => true, () => BMI.Obese],
-]);
+// Note that a switch does not support ranges, so I needed to use if/else
+export const makeBmi = (bmi) => {
+  let bmiCategory = "";
+  if (bmi <= 18.5) {
+    bmiCategory = BMI.Underweight;
+  } else if (bmi <= 25) {
+    bmiCategory = BMI.Normal;
+  } else if (bmi <= 30) {
+    bmiCategory = BMI.Overweight;
+  } else {
+    bmiCategory = BMI.Obese;
+  }
+  return bmiCategory;
+};
